9fc719713186aa2b17bc6d5ee438f43b46b0ab24
merdi elongo <merdielongo9@gmail.com>
2023-07-10 01:54:48 +0100

    fix

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_artisan_command
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterArtisanCommandPlugin.register(with: registry.registrar(forPlugin: "FlutterArtisanCommandPlugin"))
+}
